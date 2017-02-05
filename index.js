5a7e0f4e278394b4c0875a935b2d2d66c9a2b425
sebastian <sebastian.lzy@gmail.com>
2017-02-05 14:02:33 +0800

    switch to malaysia

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ const sendDataToKeen = (keenProjectId, keenWriteKey, url) => (data) => {
 module.exports.isNecessaryApiKeysProvided = isNecessaryApiKeysProvided
 
 module.exports.default = () => {
-    program.url = 'www.shopback.sg'
+    program.url = 'www.shopback.my'
 
     Promise.resolve()
     .then(isNecessaryApiKeysProvided(program, program.url))
